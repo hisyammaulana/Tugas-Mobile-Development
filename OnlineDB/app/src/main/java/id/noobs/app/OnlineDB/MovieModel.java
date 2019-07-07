9af10c2e12a7f2c61dfac2ca44cc384b5d42f463
hisyammaulana <hisyammaulana100@gmail.com>
2019-07-08 01:43:54 +0700

    Online DB

---
+++ OnlineDB/app/src/main/java/id/noobs/app/OnlineDB/MovieModel.java
@@ -0,0 +1,32 @@
+package id.noobs.app.OnlineDB;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MovieModel {
+
+    @SerializedName("title")
+    private String judulFilm;
+
+    @SerializedName("poster_path")
+    private String posterFilm;
+
+    //setter and getter
+    //klik kanan >generate>getter and setter
+
+
+    public String getJudulFilm() {
+        return judulFilm;
+    }
+
+    public void setJudulFilm(String judulFilm) {
+        this.judulFilm = judulFilm;
+    }
+
+    public String getPosterFilm() {
+        return posterFilm;
+    }
+
+    public void setPosterFilm(String posterFilm) {
+        this.posterFilm = posterFilm;
+    }
+}
